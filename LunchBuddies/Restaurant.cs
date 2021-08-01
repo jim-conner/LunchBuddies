f889873f8aa6840157abd191e52eb08ba244a79b
jim-conner <jamesdavidconner@gmail.com>
2021-08-01 10:00:00 -0500

    added notes and code examples from saturday's class

--- LunchBuddies/Restaurant.cs
+++ LunchBuddies/Restaurant.cs
@@ -12,11 +12,27 @@ namespace LunchBuddies
         private RestaurantList RestaurantList { get; set; }
         //private List<string> RestaurantsList { get; set; }
 
+        public static string RandomRestaurant()
+        {
+            var random = new Random(); //get random value first
+
+            // Jesse's version
+            var enumValues = Enum.GetValues(typeof(RestaurantList)); //get [] of enums values
+            var randomIndex = random.Next(enumValues.Length); //get random index based on length of enum array
+            var restaurantName = (RestaurantList)enumValues.GetValue(randomIndex);
+            return restaurantName.ToString();
+        }
 
         public Restaurant()
         {
-            var random = new Random();
-            List<RestaurantList> RestaurantsList = Enum.GetValues(typeof(RestaurantList))
+            var random = new Random(); //get random value first
+
+            // nathan's in-class 
+            //var RestaurantsList = Enum.GetValues<RestaurantList>();
+            //int index = random.Next(RestaurantsList.Length);
+            //RestaurantName = RestaurantsList[index].ToString();
+
+            var RestaurantsList = Enum.GetValues(typeof(RestaurantList))
                 .Cast<RestaurantList>().ToList();
             int index = random.Next(RestaurantsList.Count);
             RestaurantName = RestaurantsList[index].ToString();
